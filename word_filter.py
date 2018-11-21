c0668da136407eb327f6b1d146912a8014c641f2
Yusuke <homekawai22goal@yahoo.co.jp>
2018-11-21 11:54:49 +0900

    C2完成。 うまくできたと思う。 self.sentence.replace という｢.｣が連続する文法が正しいのか不安だった。

--- word_filter.py
+++ word_filter.py
@@ -5,6 +5,12 @@ class WordFilter():
     def detect(self, sentence):
         return self.target in sentence
 
+    def censor(self, sentence):
+        self.sentence = sentence
+        conversion = self.sentence.replace(self.target, "<consored>")
+        return conversion
+
+
 
 my_filter = WordFilter("アーセナル")
 
@@ -13,3 +19,9 @@ my_filter.detect("昨日のアーセナルの試合アツかった！")  # True�
 
 # NGワードが含まれていない場合
 my_filter.detect("昨日のリバプールの試合アツかった！")  # Falseを返す ※出力されるわけではありません！
+
+# NGワードが含まれている場合
+my_filter.censor("昨日のアーセナルの試合アツかった！")  # "昨日の<censored>の試合アツかった！" を返す ※出力されるわけではありません！
+
+# NGワードが含まれていない場合
+my_filter.censor("昨日のリバプールの試合アツかった！")  # "昨日のリバプールの試合アツかった！" を返す ※出力されるわけではありません！
